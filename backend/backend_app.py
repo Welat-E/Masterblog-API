8a32b37e47d87a9934fa391b4d0a28a1b25bf1ea
Welat-E <welaterenug@gmail.com>
2024-09-04 18:14:06 +0200

    Created search func.

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -49,6 +49,19 @@ def update(id):
     return jsonify({"error": "Post not found"}), 404
 
 
+@app.route('/api/posts/search', methods=['GET'])
+def search():
+    title_query = request.args.get('title', '')
+    content_query = request.args.get('content', '')
+    
+    results = []
+    for post in POSTS:
+        if title_query.lower() in post['title'].lower() or content_query.lower() in post['content'].lower():
+            results.append(post)
+    
+    return jsonify(results)
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5002, debug=True)
 
